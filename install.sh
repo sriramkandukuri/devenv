57a5740b39628a2aae54fb2e27fa3caee93efcde
Sriram.K <sriramkandukuri449@gmail.com>
2020-04-26 20:21:42 +0530

    Updated colors for vim

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ then
     exit -1
 fi
 
+rm -rf build
 mkdir build
 cd build
 
@@ -27,41 +28,54 @@ install_tmux ()
 # install tmux conf
 install_tmux_conf ()
 {
-mkdir tmuxconf
-cd tmuxconf
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf ~/.otmux.conf
-ln -s -f .tmux/.tmux.conf.local ~/
-ln -s $curdir/mytmux.conf ~/.tmux.conf
-cd $builddir
+    mkdir tmuxconf
+    cd tmuxconf
+    git clone https://github.com/gpakosz/.tmux.git
+    unlink ~/.otmux.conf
+    unlink ~/.tmux.conf.local
+    unlink ~/.tmux.conf
+    ln -s -f .tmux/.tmux.conf ~/.otmux.conf
+    ln -s -f .tmux/.tmux.conf.local ~/
+    ln -s $curdir/mytmux.conf ~/.tmux.conf
+    cd $builddir
 }
 
 # install bashrc
 install_bashrc ()
 {
-echo "Add '. ~/devenv/bashrc'  to ~/.bashrc"
+    echo "Add '. ~/devenv/bashrc'  to ~/.bashrc"
+}
+
+install_vim ()
+{
+    sudo add-apt-repository ppa:jonathonf/vim
+    sudo apt update
+    sudo apt install vim
 }
 
 # get vimrc and install plugins
 install_vimrc ()
 {
-mkdir vimrc
-cd vimrc
-git clone https://github.com/erkrnt/awesome-streamerrc.git
-cp awesome-streamerrc/ThePrimeagen/.vimrc $curdir/open_vimrc
-ln -s $curdir/open_vimrc ~/.ovrc
-ln -s $curdir/myvimrc ~/.vimrc
+    mkdir vimrc
+    cd vimrc
+    git clone https://github.com/erkrnt/awesome-streamerrc.git
+    cp awesome-streamerrc/ThePrimeagen/.vimrc $curdir/open_vimrc
+    unlink ~/ovrc
+    unlink ~/.vimrc
+    ln -s $curdir/open_vimrc ~/.ovrc
+    ln -s $curdir/myvimrc ~/.vimrc
 
-# Install vim plug plugin manager
-## remove already installed .vim directory
-unlink ~/.vim
-rm -rf ~/.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    # Install vim plug plugin manager
+    ## remove already installed .vim directory
+    unlink ~/.vim
+    rm -rf ~/.vim
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
+    vim +PlugInstall +qall
 }
 
 install_bashrc
 install_tmux
 install_tmux_conf
+intall_vim
 install_vimrc
