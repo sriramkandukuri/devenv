69ef7fc48b113bd09ce6f5e5438612b7290567a8
Sriram.K <sriramkandukuri449@gmail.com>
2020-05-06 17:46:35 +0530

    color palette for dracula added in other files for reference

---
+++ other-files/colorpalette_dracula.sh
@@ -0,0 +1,23 @@
+# Background #282a36 RGB{40,42,54}
+# Text color #f8f8f2 RGB{248 248 242}
+
+[
+  "#21222C",
+  "#FF5555",
+  "#50FA7B",
+  "#F1FA8C",
+  "#BD93F9",
+  "#FF79C6",
+  "#8BE9FD",
+  "#F8F8F2",
+  "#6272A4",
+  "#FF6E6E",
+  "#69FF94",
+  "#FFFFA5",
+  "#D6ACFF",
+  "#FF92DF",
+  "#A4FFFF",
+  "#FFFFFF"
+]
+
+#
