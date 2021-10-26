cee6a63fb9b8861de94699ed70d8ec3da231a29c
Sriram Kandukuri <sriramkandukuri449@gmail.com>
2021-10-27 02:28:51 +0530

    remove lualine x section to enable default

--- vim/lua/devenv/lualine.lua
+++ vim/lua/devenv/lualine.lua
@@ -11,7 +11,6 @@ require('lualine').setup {
             { 'mode', separator = { left = '', right = '' }, right_padding = 2 },
         },
         lualine_c = { "filename", "vim.b.lsp_current_function" },
-        lualine_x = { "" },
         lualine_z = {
             { 'location', separator = { right = '', left = '' }, left_padding = 2 },
         },
