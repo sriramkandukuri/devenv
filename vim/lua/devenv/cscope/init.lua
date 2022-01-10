30760083657cb6952322193a241811fae45096fc
Sriram.K <sriramkandukuri449@gmail.com>
2022-01-11 01:20:44 +0530

    vim:cscope: add more functions to lua implementation
    Signed-off-by: Sriram.K <sriramkandukuri449@gmail.com>

--- vim/lua/devenv/cscope/init.lua
+++ vim/lua/devenv/cscope/init.lua
@@ -18,22 +18,50 @@ local show = function(cmd)
 end
 
 local M = {}
-
-M.findsymbol = function()
+local x = function(c,word)
     local srcfile = vim.api.nvim_buf_get_name(0)
     vim.fn.setqflist({})
-    local word = vim.fn.expand "<cword>"
-    local cmd = "silent keepjumps execute \"cs f s "..word.."\""
-    -- vim.cmd([[normal! mY]])
+    local cmd = "cs find ".." "..c.." "..word
+    vim.cmd([[normal! mY]])
     -- local view = vim.fn.winsaveview()
     vim.cmd(cmd)
     vim.cmd([[cclose]])
     local curfile = vim.api.nvim_buf_get_name(0)
     if curfile ~= srcfile then
-        vim.api.nvim_buf_delete(0, {force = true})
-        -- vim.cmd([[normal! `Y]])
+        -- vim.api.nvim_buf_delete(0, {force = true})
+        vim.cmd([[normal! `Y]])
         -- vim.fn.winrestview( view )
     end
     show(cmd)
 end
+
+M.cscopefn = function(c)
+    local word = vim.fn.expand "<cword>"
+    return x(c, word)
+end
+
+M.cscopefilefn = function(c)
+    local word = vim.fn.expand "<cfile>:t"
+    return x(c, word)
+end
+
+M.symbols = function()
+    M.cscopefn("s")
+end
+M.callers = function()
+    M.cscopefn("c")
+end
+M.files = function()
+    M.cscopefilefn("f")
+end
+M.functions = function()
+    M.cscopefn("d")
+end
+M.includes = function()
+    M.cscopefilefn("i")
+end
+M.assignments = function()
+    M.cscopefn("a")
+end
+
 return M
